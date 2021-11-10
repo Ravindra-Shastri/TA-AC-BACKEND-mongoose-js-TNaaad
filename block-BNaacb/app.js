df15821cf808b223abab71c6f871fd32ecd547ad
Ravindra-Shastri <ravindrashastri90@gmail.com>
2021-11-10 15:22:54 +0530

    index.md

--- block-BNaacb/app.js
+++ block-BNaacb/app.js
@@ -1,7 +1,20 @@
 var express = require('express');
 
+var mongoose = require('mongoose');
+
+mongoose.connect(
+    "mongodb://localhost/sample",
+    {useNewUrlParser: true, useUnifiedTopology: true},
+    (err) => {
+        console.log(err ? err : "connected true");
+    }
+)
+
 var app = express();
 
+app.get('/', (req, res) => {
+    res.send('welcome');
+});
 
 app.listen(3000, () => {
     console.log('server listning on port 3k')
