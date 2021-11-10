66b03761e356ae0e93e13c850e12d00b9597b272
Ravindra-Shastri <ravindrashastri90@gmail.com>
2021-11-10 11:20:53 +0530

    index.md

--- block-BNaabz/app.js
+++ block-BNaabz/app.js
@@ -1,3 +1,18 @@
 var express = require('express');
+var mongoose = require('mongoose');
 
-var server = express();
+//connect to Mongodb
+
+mongoose.connect('mongodb://localhost/sample', (err) => {
+    console.log(err ? err : 'connected true')
+})
+
+var app = express();
+
+app.get('/', (req, res) => {
+    res.send('welcome');
+});
+
+app.listen(3000, () => {
+    console.log('server is listening on port 3k');
+})
